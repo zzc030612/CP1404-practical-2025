6b558054ae80f3bb8a4f26f1f0c5701c9b67fa21
zzc030612 <2683332593@qq.com>
2025-06-14 22:48:54 +0800

    Extracted last names of full names longer than 11 characters and joined them into a string

--- prac_04/list_comprehensions.py
+++ prac_04/list_comprehensions.py
@@ -46,3 +46,5 @@ print(greater_numbers)
 # TODO: (more advanced) use a list comprehension and the join string method
 # to create a string (not list) of the last names for those full names longer than 11 characters
 # the result should be: 'Harlem, Hendrix, Lovelace'
+long_last_names = [name.split()[1] for name in full_names if len(name) > 11]
+print(long_last_names)
