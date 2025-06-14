b67019be72fc2cc3e8730979799a92bfc9da5b9e
zzc030612 <2683332593@qq.com>
2025-06-14 22:29:40 +0800

    Added a security checker to verify usernames against a predefined list

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -8,3 +8,14 @@ print(f"The last number is {numbers[-1]}")
 print(f"The smallest number is {min(numbers)}")
 print(f"The largest number is {max(numbers)}")
 print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+input_name = input("Enter your name: ")
+while input_name != "":
+    if input_name in usernames:
+        print("Access granted")
+    else:
+        print("Access denied")
+    input_name = input("Entry your name: ")
