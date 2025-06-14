48c8c0f47201813832ec057ae84e2002ff6521f6
zzc030612 <2683332593@qq.com>
2025-06-14 22:47:58 +0800

    Filtered numbers greater than 9 using list comprehension

--- prac_04/list_comprehensions.py
+++ prac_04/list_comprehensions.py
@@ -40,6 +40,8 @@ print(numbers)
 
 # TODO: list comprehension to create a list of only the numbers that are
 # greater than 9 from the numbers (not strings) you just created
+greater_numbers = [number for number in numbers if number > 9]
+print(greater_numbers)
 
 # TODO: (more advanced) use a list comprehension and the join string method
 # to create a string (not list) of the last names for those full names longer than 11 characters
