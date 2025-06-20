470f18bc927ec0a01c3f926d9de2128a5b17f00a
zzc030612 <2683332593@qq.com>
2025-06-21 03:52:59 +0800

    Implement champion win-count statistics

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -10,3 +10,12 @@ def load_data(filename):
     with open(filename, "r", encoding="utf-8-sig") as in_file:
         lines = in_file.readlines()
     return [line.strip().split(',') for line in lines[1:]]  # Skip header
+
+
+def get_champions(data):
+    """Return dictionary of champions and their win counts."""
+    champions = {}
+    for row in data:
+        name = row[2]  # Champion name column
+        champions[name] = champions.get(name, 0) + 1
+    return champions
