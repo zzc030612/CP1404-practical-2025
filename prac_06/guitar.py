752e417621fd40e288235ef02d6708ebb2805f1c
zzc030612 <2683332593@qq.com>
2025-07-12 21:49:38 +0800

    Implemented get_age method to calculate guitar age.

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -17,3 +17,8 @@ class Guitar:
     def __str__(self):
         """Return a string representation of the Guitar."""
         return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
+
+    def get_age(self):
+        """Get the age of the guitar in years."""
+        current_year = 2023  # Update this to the current year if needed
+        return current_year - self.year
