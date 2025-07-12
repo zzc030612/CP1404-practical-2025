e5c38acab153be5fe5dcfb8b9f70fe1e495f4439
zzc030612 <2683332593@qq.com>
2025-07-12 19:39:57 +0800

    Added fields: name, typing, reflection, and year. Implemented __init__ method to initialize the fields.

---
+++ prac_06/programming_language.py
@@ -0,0 +1,9 @@
+class ProgrammingLanguage:
+    """Represent a programming language with its attributes."""
+
+    def __init__(self, name, typing, reflection, year):
+        """Initialize a ProgrammingLanguage instance."""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
