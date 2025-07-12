784c21dc291db3c10e58ffb01a2636eabb491e91
zzc030612 <2683332593@qq.com>
2025-07-12 21:48:25 +0800

    Added attributes: name, year, cost. Implemented __init__ method to initialize guitar details.

---
+++ prac_06/guitar.py
@@ -0,0 +1,15 @@
+"""
+Guitar class for storing details about guitars.
+
+Estimated time: 20 minutes
+Actual time: 15 minutes
+"""
+
+class Guitar:
+    """Represent a guitar."""
+
+    def __init__(self, name="", year=0, cost=0):
+        """Initialize a Guitar instance."""
+        self.name = name
+        self.year = year
+        self.cost = cost
