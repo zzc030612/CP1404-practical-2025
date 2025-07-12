4e1147741e942d7aaa8555ed9591b5ba86c081ec
zzc030612 <2683332593@qq.com>
2025-07-12 21:55:28 +0800

    Tested is_vintage method with expected and actual outputs.

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -17,6 +17,10 @@ def main():
     print(f"{gibson.name} get_age() - Expected 101. Got {gibson.get_age()}")
     print(f"{another_guitar.name} get_age() - Expected 10. Got {another_guitar.get_age()}")
 
+    # Test is_vintage()
+    print(f"{gibson.name} is_vintage() - Expected True. Got {gibson.is_vintage()}")
+    print(f"{another_guitar.name} is_vintage() - Expected False. Got {another_guitar.is_vintage()}")
+
 
 if __name__ == "__main__":
     main()
