362c4c018f7cb1690b04dbe10fbb6215353fe9f7
zzc030612 <2683332593@qq.com>
2025-07-13 01:15:29 +0800

    Implemented Guitar class with attributes: name, year, cost.

---
+++ prac_07/guitar.py
@@ -0,0 +1,25 @@
+"""
+Guitar class for representing a guitar object.
+"""
+
+class Guitar:
+    """Represent a Guitar object."""
+
+    def __init__(self, name, year, cost):
+        """Initialize a Guitar instance."""
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """Return a string representation of the Guitar."""
+        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
+
+    def get_age(self):
+        """Get the age of the guitar in years."""
+        current_year = 2023  # Update this to the current year if needed
+        return current_year - self.year
+
+    def is_vintage(self):
+        """Determine if the guitar is vintage (50+ years old)."""
+        return self.get_age() >= 50
