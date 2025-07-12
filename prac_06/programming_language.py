26aca4d22d10a2739a7e5ddc69798ddb632f4773
zzc030612 <2683332593@qq.com>
2025-07-12 19:41:22 +0800

    Implemented __str__ method to provide a readable string representation of the language.

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -11,3 +11,7 @@ class ProgrammingLanguage:
     def is_dynamic(self):
         """Check if the programming language is dynamically typed."""
         return self.typing.lower() == "dynamic"
+
+    def __str__(self):
+        """Return a string representation of the programming language."""
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
