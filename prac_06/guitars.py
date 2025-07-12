61ae41f87a97db6ebb580d1d885b9d4ed3ef9e5d
zzc030612 <2683332593@qq.com>
2025-07-12 22:00:58 +0800

    Displayed all guitars with formatted details, including vintage status.

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -21,3 +21,12 @@ def main():
         guitar = Guitar(name, year, cost)
         guitars.append(guitar)
         print(f"{guitar} added.\n")
+
+    # Display all guitars
+    print("\nThese are my guitars:")
+    for i, guitar in enumerate(guitars, 1):
+        vintage_string = " (vintage)" if guitar.is_vintage() else ""
+        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+
+if __name__ == "__main__":
+    main()
