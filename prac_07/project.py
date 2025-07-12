d38937bebdd76ee1afeee8ad7fb46fcf9a93cf30
zzc030612 <2683332593@qq.com>
2025-07-13 01:42:15 +0800

    Implemented Project class with attributes: name, start_date, priority, cost_estimate, completion_percentage.

---
+++ prac_07/project.py
@@ -0,0 +1,17 @@
+"""
+Project class for representing a project.
+"""
+
+from datetime import datetime
+
+
+class Project:
+    """Represent a project."""
+
+    def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
+        """Initialize a Project instance."""
+        self.name = name
+        self.start_date = datetime.strptime(start_date, "%d/%m/%Y").date()
+        self.priority = int(priority)
+        self.cost_estimate = float(cost_estimate)
+        self.completion_percentage = int(completion_percentage)
