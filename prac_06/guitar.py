14f4c31ec49ed8c317886e7f568e8572a4d06ff2
zzc030612 <2683332593@qq.com>
2025-07-12 21:49:00 +0800

    Added __str__ method for formatted string representation.

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -13,3 +13,7 @@ class Guitar:
         self.name = name
         self.year = year
         self.cost = cost
+
+    def __str__(self):
+        """Return a string representation of the Guitar."""
+        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
