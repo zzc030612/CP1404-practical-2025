ca9b1ff93d8654c6c2d097fc8387be86764e8f7d
zzc030612 <2683332593@qq.com>
2025-07-17 17:41:10 +0800

    Set custom window size to (500, 100) for better UI presentation.

--- prac_08/squaring.py
+++ prac_08/squaring.py
@@ -1,7 +1,11 @@
 from kivy.app import App
+from kivy.core.window import Window
 
 __author__ = 'Zheng Zhicong'
 
 
 class SquareNumberApp(App):
     """ SquareNumberApp is a Kivy App for squaring a number """
+    def build(self):
+        """ build the Kivy app from the kv file """
+        Window.size = (500, 100)
