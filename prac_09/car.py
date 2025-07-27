675bb8be99de012a4bc577cc16d0b77b339164bf
zzc030612 <2683332593@qq.com>
2025-07-27 20:11:13 +0800

    copy the finished version in the solutions to prac_09 folder

--- prac_09/car.py
+++ prac_09/car.py
@@ -1,20 +1,19 @@
-"""CP1404/CP5632 Practical - Car class example."""
-
-
 class Car:
     """Represent a Car object."""
 
-    def __str__(self):
-        return f"{self.name}, fuel={self.fuel}, odometer={self._odometer}"
-
-    def __init__(self, fuel=0, name=""):
+    def __init__(self, name="", fuel=0):
         """Initialise a Car instance.
 
+        name: string, reference name for car
         fuel: float, one unit of fuel drives one kilometre
         """
         self.name = name
         self.fuel = fuel
-        self._odometer = 0
+        self.odometer = 0
+
+    def __str__(self):
+        """Return a string representation of a Car object."""
+        return f"{self.name}, fuel={self.fuel}, odometer={self.odometer}"
 
     def add_fuel(self, amount):
         """Add amount to the car's fuel."""
@@ -31,5 +30,5 @@ class Car:
             self.fuel = 0
         else:
             self.fuel -= distance
-        self._odometer += distance
+        self.odometer += distance
         return distance
